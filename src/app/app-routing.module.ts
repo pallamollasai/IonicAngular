5d3550313dc20d6ce60d8d18f4e387ab82616ba4
Pallamolla Sai <33921512+pallamollasai@users.noreply.github.com>
2019-09-08 22:48:41 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  // { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   { path: 'list', loadChildren: () => import('./list/list.module').then( m => m.ListPageModule)},
   // { path: 'list', loadChildren: './list/list.module#ListPageModule' },
